27681f3b626b29492d940ecb45e7e5cc40010764
Harvey Hu <harveyhu@microsoft.com>
2018-07-04 18:25:20 +0800

    razor compile and run test

--- PowerWorkflow/Common/RazorHelper.cs
+++ PowerWorkflow/Common/RazorHelper.cs
@@ -12,25 +12,25 @@ namespace PowerWorkflow.Common
 {
     public class RazorHelper
     {
-        public static string Parse<T>(HttpContext context, string virtualPath, T Model)
-        {
-            string path = context.Server.MapPath(virtualPath);
-            string templateSource = File.ReadAllText(path);
-            //设置cacheName缓存。
-            string cacheName = path + File.GetLastWriteTime(path);
+        //public static string Parse<T>(HttpContext context, string virtualPath, T Model)
+        //{
+        //    string path = context.Server.MapPath(virtualPath);
+        //    string templateSource = File.ReadAllText(path);
+        //    //设置cacheName缓存。
+        //    string cacheName = path + File.GetLastWriteTime(path);
 
 
 
-            //string result = Razor.Parse<T>(rawhtml, Model, cacheName);
+        //    //string result = Razor.Parse<T>(rawhtml, Model, cacheName);
 
-            var result = RazorEngine.Engine.Razor.RunCompile(templateSource, cacheName, null, Model);
+        //    var result = RazorEngine.Engine.Razor.RunCompile(templateSource, cacheName, null, Model);
 
-            return result;
-        }
+        //    return result;
+        //}
 
 
 
-        public static string Parse(string templateContent, object Model, string templateKey = "defaultTemplate")
+        public static string Parse<T>(string templateContent, T Model, string templateKey = "defaultTemplate")
         {
 
 
---
+++ PowerWorkflowTests/Json/TestJson.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using PowerWorkflow.Common;
+using PowerWorkflow.Workflow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerWorkflowTests.Json
+{
+    [TestClass()]
+    public class TestJsonTests
+    {
+        [TestMethod()]
+        public void JObjectTest()
+        {
+
+            var s = Newtonsoft.Json.JsonConvert.SerializeObject(new Foo { Name = "abc", age = 10 });
+
+            // var o = "{name:'ada'}";
+             var o = JsonConvert.DeserializeObject< Foo>(s);
+
+
+            //var m = RazorHelper.Parse<Foo>("<h1>say @Model.Name</h1>", new Foo { Name = "abc", age = 10 } );
+
+            var m =
+              //RazorHelper.Parse("<h1>@Model.Name</h1>", new { Name = "Jason" });
+              RazorHelper.Parse("<h1>@Model.Name</h1>", new   { Name = "abc", age = 10 });
+            Console.WriteLine(m);
+
+        }
+
+
+    }
+
+    internal class Foo
+    {
+        public string Name;
+        public int age { get; set; }
+    }
+}
